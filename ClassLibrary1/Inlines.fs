4359d69eb739f2ce99089787a9c5836c3bfc08e6
Mark Lambert <mark.lambert@biosignatures.com>
2020-01-06 11:35:11 +0000

    Demo of code coverage problems with inlining

---
+++ ClassLibrary1/Inlines.fs
@@ -0,0 +1,16 @@
+﻿namespace ClassLibrary1
+
+module Inlines =
+    let inline addUp (xs : ^t list) =
+        match xs with
+        | h :: t ->
+            let mutable result = h
+
+            // Change this loop to
+            //  for x in xs do
+            // And the test using the quotation will re-run and fail, but the other two tests will still show as passing
+            for x in t do
+                result <- result + x
+            result
+        | [] -> failwith "Nothing to add"
+
---
+++ Tests/TestInlines.fs
@@ -0,0 +1,23 @@
+﻿namespace Tests
+
+open NUnit.Framework
+open FsUnitTyped
+open Swensen.Unquote
+
+open ClassLibrary1
+
+type [<Measure>] m
+
+[<TestFixture>]
+type TestInlines() =
+    [<Test>]
+    member __.Test1() =
+        Inlines.addUp [1.0;2.0;3.0] |> shouldEqual 6.0
+
+    [<Test>]
+    member __.Test2() =
+        Inlines.addUp [1<m>;2<m>;3<m>] |> shouldEqual 6<m>
+
+    [<Test>]
+    member __.Test3() =
+        test <@ Inlines.addUp [1<m>;2<m>;3<m>]  = 6<m> @>
